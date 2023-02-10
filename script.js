b348942dbd5207773d469dfd507a60f0f2453760
Anmol Kayastha <74373013+anmolkayastha@users.noreply.github.com>
2023-02-09 20:57:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ const billInputEl = document.getElementById('bill');
 const tipAmtEl = document.getElementById('tip-amt-num');
 const totalAmtEl = document.getElementById('total-amt-num');
 
-// Take input from user
-// Validate input
-    // Check if there are letters in input value
-    // If yes, show an error message
-    // If not, then take the input value 
+const tip_5_El = document.getElementById('tip-5');
+const tip_10_El = document.getElementById('tip-10');
+const tip_15_El = document.getElementById('tip-15');
+const tip_20_El = document.getElementById('tip-20');
+const tip_custom_El = document.getElementById('tip-custom');
+
+
     
 // Flag for whether error message was displayed
 let fired = false;
 
 // When user presses enter, validate input
+/*
 billInputEl.addEventListener("keyup", function(event){
     event.preventDefault();
     if(event.key === 'Enter'){
@@ -22,13 +25,41 @@ billInputEl.addEventListener("keyup", function(event){
         validateInput(billValue);
     }
 });
+*/
 
+// When tip button is clicked
+    // Validate input
+    // Calculate tip
+    // Show total
+
+tip_5_El.addEventListener('click', function(event){
+    if(validateInput(billInputEl.value)){
+        billSubTotal = parseFloat(billInputEl.value);
+        let tip_amt = billSubTotal * 0.05;
+        updateTipAmt(tip_amt);
+        updateTotalAmt(tip_amt, billSubTotal);
+    }
+});
+
+function updateTipAmt(tip_amt){
+    tipAmtEl.innerHTML  = '$' + tip_amt.toFixed(2);
+}
+
+function updateTotalAmt(tip_amt, subTotal){
+    let totalAmt = tip_amt + subTotal;
+    totalAmtEl.innerHTML = '$' + totalAmt.toFixed(2);
+}
+
+    
+
+// Check whether input is a number and display erorr messages if needed
 function validateInput(billSubTotal){
     if (isNaN(parseFloat(billSubTotal))){
         if(!fired){
             showErrorMsg();
             fired = true;
         }
+        return false;
     }
     // if input is okay
     else{   
@@ -37,10 +68,11 @@ function validateInput(billSubTotal){
             removeErrorMsg();
             fired = false;
         }
-        // return input as number
-        return parseFloat(billSubTotal); 
+        return true;
     }
 }
+
+// Display error message
 function showErrorMsg(){
     let span = document.createElement('span');
     span.textContent = "Please enter a number";
@@ -50,6 +82,7 @@ function showErrorMsg(){
     console.log("function running");
 }
 
+// Remove error message
 function removeErrorMsg(){
  let errorMsg = document.getElementById('error');
    errorMsg.remove();
